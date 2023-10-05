809da79254dbcfd0e802341ed35407d6db72a290
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-10-04 22:09:25 -0300

    Project created

---
+++ src/test/java/com/lucasweb/cource/CourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasweb.cource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
