f96a95df189c82e07dcfb65103e7a3e5f738796b
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-10-06 20:20:34 -0300

    Service layer, component registration

---
+++ src/main/java/com/lucasweb/cource/services/UserService.java
@@ -0,0 +1,25 @@
+package com.lucasweb.cource.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasweb.cource.entities.User;
+import com.lucasweb.cource.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
